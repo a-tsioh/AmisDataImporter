4b71a90e4056a505662ce330c63544a225e2316e
pierre <pierre@magistry.fr>
2018-05-16 23:41:26 +0800

    first try for foldr.padagraph.io 2018

--- importer.py
+++ importer.py
@@ -2,19 +2,19 @@
 import json
 from collections import namedtuple
 
-from botapi import Botagraph, BotApiError
-from reliure.types import Text
+#from botapi import Botagraph, BotApiError
+#from reliure.types import Text
 
 NodeType = namedtuple("NodeType", "name description properties")
 EdgeType = namedtuple("EdgeType", "name description properties")
 
 
 # data source
-data = json.load(open("./data/dict-amis.json"))
+data = json.load(open("/tmp/dict-amis-safolu.json"))
 
 # Graph Definition
 
-PDG_HOST = "http://g0v-tw.padagraph.io"
+PDG_HOST = "http://foldr.padagraph.io/dict-amis"
 PDG_KEY = ""
 GRAPHNAME = "Amis Dict"
 DESCRIPTION = "lexical graph built from Amis Dictionnaries"
@@ -23,15 +23,14 @@ TAGS = ["Amis", "lexicon"]
 
 # Node Types definitions
 
-NodeWordForm = NodeType("Form", "", {"id": Text(),
-                                     "label": Text()})
-NodeStem = NodeType("Stem", "", {"id": Text(),"label": Text()})
-NodeLexem = NodeType("Lexeme", "", {"id": Text(),
-                                    "label": Text(),
-                                    "form": Text(),
-                                    "stem": Text(),
-                                      "definition": Text(),
-                                      "examples": Text()})
+NodeWordForm = NodeType("Form", "", ["id", "label"])
+NodeStem = NodeType("Stem", "", ["id", "label"])
+NodeLexem = NodeType("Lexeme", "", ["id",
+                                    "label",
+                                    "form",
+                                    "stem",
+                                    "definition",
+                                    "examples"])
 
 # Edge Types definitions
 
@@ -42,19 +41,9 @@ EdgeHeteronymy  = EdgeType("heteronym", "", {})
 
 # Graph creation
 
-bot = Botagraph(PDG_HOST, PDG_KEY)
-bot.create_graph(GRAPHNAME, {'description': DESCRIPTION, "tags": TAGS})
 
-
-# Posting Nodes and Edges Types
-
-nodetypes_uuids = {}
-edgetypes_uuids = {}
-for nt in [NodeStem, NodeLexem, NodeWordForm]: 
-    nodetypes_uuids[nt.name] = bot.post_nodetype(GRAPHNAME, nt.name, nt.description, nt.properties)
-
-for et in [EdgeStem, EdgeSynonyms, EdgeHeteronymy]:
-    edgetypes_uuids[et.name] = bot.post_edgetype(GRAPHNAME, et.name, et.description, et.properties)
+#for nt in [NodeStem, NodeLexem, NodeWordForm]: 
+#for et in [EdgeStem, EdgeSynonyms, EdgeHeteronymy]:
 
 
 # Posting Nodes
@@ -62,15 +51,15 @@ for et in [EdgeStem, EdgeSynonyms, EdgeHeteronymy]:
 def getNodesIterator():
     for entry in data:
         form = entry['title']
-        yield { 'nodetype': nodetypes_uuids[NodeWordForm.name],
-                'properties': {'id': (NodeWordForm.name, form),
-                    'label': form}}
+        #yield { 'nodetype': NodeWordForm.name,
+        #        'properties': {'id': (NodeWordForm.name, form),
+        #            'label': form}}
         stem = ""
         if 'stem' in entry:
             stem = entry['stem']
-            yield { 'nodetype': nodetypes_uuids[NodeStem.name],
-                'properties': {'id': (NodeStem.name, stem),
-                    'label': stem}}
+            #yield { 'nodetype': NodeStem.name,
+            #    'properties': {'id': (NodeStem.name, stem),
+            #        'label': stem}}
         i = 0
         for het in entry['heteronyms']:
             for lex in het['definitions']:
@@ -80,52 +69,14 @@ def getNodesIterator():
                 if "example" in lex:
                     examples = "\n".join([x.replace(u"\ufff9","- ")
                             .replace(u"\ufffa","").replace(u"\ufffb"," ") for x in lex['example']])
-                props = {'form': form,
-                        'id': (NodeLexem.name, lexid),
-                        'label': lexid,
-                        'stem': stem,
-                        'definition': lex['def'],
-                        'examples': examples}
-                yield { 'nodetype': nodetypes_uuids[NodeLexem.name],
+                props = [form,
+                        "-".join((NodeLexem.name, lexid)),
+                        lexid,
+                        stem,
+                        lex['def'],
+                        examples]
+                yield { 'nodetype': NodeLexem.name,
                         'properties': props}
 
-
-
-
-nodes_uuids = {}
-for node, uuid in bot.post_nodes(GRAPHNAME, getNodesIterator()):
-    nid = node['properties']['id']
-    nodes_uuids[nid] = uuid
-
-# Posting Edges
-
-def getEdgesIterator():
-    for entry in data:
-        form = entry['form']
-        form_id = nodes_uuids[(NodeWordForm.name, form)]
-        if 'stem' in entry:
-            stem = entry['stem']
-            stem_id = nodes_uuids[(NodeStem.name, stem)]
-            yield {'edgetype': edgetypes_uuids[EdgeStem.name],
-                    'source': form_id,
-                    'target': stem_id,
-                    'properties': {}}
-        i = 0
-        for het in entry['heteronyms']:
-            for lex in het['definitions']:
-                i += 1 
-                lexid = nodes_uuids[(NodeLexem.name,"%s-%d" % (form,i))]
-                yield {'edgetype': edgetypes_uuids[EdgeHeteronymy.name],
-                        'source': form_id,
-                        'target': lexid,
-                        'properties': {}}
-                for syn in lex.get('synonyms',[]):
-                    syn_id = uuid.get((NodeWordForm.name, syn), None)
-                    if syn_id is not None:
-                        yield {'edgetype': edgetypes_uuids[EdgeSynonyms.name],
-                                'source': form_id,
-                                'target': syn_id,
-                                'properties': {}}
-
-
-list(bot.post_edges(GRAPHNAME, getEdgesIterator()))
+for n in getNodesIterator():
+    print(", ".join(n['properties']))
